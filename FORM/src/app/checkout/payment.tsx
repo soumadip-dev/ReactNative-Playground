11d73d0adfdf4ae49b4928c9d948651f955d19d2
soumadip-dev <soumadipmajila@gmail.com>
2026-05-04 12:00:00 +0530

    feat: complete payment screen implementation with proper Zod validation integration

--- FORM/src/app/checkout/payment.tsx
+++ FORM/src/app/checkout/payment.tsx
@@ -1,20 +1,59 @@
 import CustomButton from '@/src/components/CustomeButton';
 import { router } from 'expo-router';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import KeyboardAwareScrollView from '@/src/components/KeyboardAwareScrollView';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
+import ReactHookFormTextInput from '@/src/components/ReactHookFormTextInput';
+import { PaymentInfo, PaymentDetailsSchema } from '@/src/schemas/payment.schema';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export default function PaymentDetailsForm() {
-  function handleNextStep() {
-    // Validate the form
-    // If valid, navigate to the next screen
+  const form = useForm<PaymentInfo>({
+    resolver: zodResolver(PaymentDetailsSchema),
+  });
+
+  const { handleSubmit } = form;
+
+  const handleNextStep: SubmitHandler<PaymentInfo> = data => {
+    console.log('DATA FROM THE FORM:', data);
     router.push('/checkout/confirm');
-  }
+  };
 
   return (
     <KeyboardAwareScrollView>
-      <Text>Payment Screen.</Text>
-
-      <CustomButton title="Next" style={styles.nextButton} onPress={handleNextStep} />
+      <FormProvider {...form}>
+        <ReactHookFormTextInput
+          placeholder="1234 1234 1234 1234"
+          labelText="Card Number"
+          name="cardNumber"
+          inputMode="numeric"
+        />
+        <View style={styles.cardDetailsRow}>
+          <ReactHookFormTextInput
+            placeholder="MM/YY"
+            labelText="Expiry Date"
+            name="expiryDate"
+            wrapperStyle={styles.flexInput}
+          />
+          <ReactHookFormTextInput
+            placeholder="CVV"
+            labelText="CVV"
+            name="cvv"
+            inputMode="numeric"
+            wrapperStyle={styles.flexInput}
+          />
+        </View>
+        {/* checkbox - Save your card information */}
+        <View style={styles.saveCardContainer}>
+          <View style={styles.checkboxPlaceholder} />
+          <Text style={styles.saveCardText}>Save card information</Text>
+        </View>
+        <CustomButton
+          title="Next"
+          style={styles.nextButton}
+          onPress={handleSubmit(handleNextStep)}
+        />
+      </FormProvider>
     </KeyboardAwareScrollView>
   );
 }
@@ -22,6 +61,30 @@ export default function PaymentDetailsForm() {
 const styles = StyleSheet.create({
   nextButton: {
     marginTop: 'auto',
-    marginBottom: 25,
+  },
+  cardDetailsRow: {
+    flexDirection: 'row',
+    gap: 5,
+    marginTop: 15,
+  },
+  flexInput: {
+    flex: 1,
+  },
+  saveCardContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 16,
+    gap: 10,
+  },
+  checkboxPlaceholder: {
+    width: 20,
+    height: 20,
+    borderWidth: 1,
+    borderRadius: 4,
+    marginLeft: 10,
+  },
+
+  saveCardText: {
+    fontSize: 14,
   },
 });
---
+++ FORM/src/schemas/payment.schema.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod';
+
+const PaymentDetailsSchema = z.object({
+  cardNumber: z
+    .string({ message: 'Card number is required' })
+    .min(13, { message: 'Card number must be at least 13 digits' })
+    .max(19, { message: 'Card number must be at most 19 digits' })
+    .regex(/^\d{13,19}$/, {
+      message: 'Please enter a valid card number',
+    })
+    .trim(),
+
+  expiryDate: z
+    .string({ message: 'Expiry date is required' })
+    .regex(/^(0[1-9]|1[0-2])\/\d{2}$/, {
+      message: 'Expiry date must be in MM/YY format',
+    })
+    .trim(),
+
+  cvv: z
+    .string({ message: 'cvv is required' })
+    .trim()
+    .min(3, { message: 'CVV must be at least 3 digits' })
+    .max(4, { message: 'CVV must be at most 4 digits' }),
+});
+
+export type PaymentInfo = z.infer<typeof PaymentDetailsSchema>;
+
+export { PaymentDetailsSchema };
