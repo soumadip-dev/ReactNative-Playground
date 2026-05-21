c253c5d2cafa3e1ddbf8d818bcbd40ddfc1315f3
soumadip-dev <soumadipmajila@gmail.com>
2026-05-21 14:43:55 +0530

    feat: integrate Immer middleware with Zustand for cleaner and more maintainable state updates

--- 05_FITNESS_PROJECT/src/store/index.ts
+++ 05_FITNESS_PROJECT/src/store/index.ts
@@ -2,6 +2,7 @@ import { WorkoutWithExercises } from '@/types/models';
 import { create } from 'zustand';
 import { finishedWorkout, newWorkout } from '@/services/workoutService';
 import { createExercise } from '@/services/exerciseService';
+import { immer } from 'zustand/middleware/immer';
 
 type State = {
   currentWorkout: WorkoutWithExercises | null;
@@ -14,40 +15,47 @@ type Actions = {
   addExercise: (name: string) => void;
 };
 
-export const useWorkout = create<State & Actions>()((set, get) => ({
-  currentWorkout: null,
-  workouts: [],
+export const useWorkout = create<State & Actions>()(
+  immer((set, get) => ({
+    currentWorkout: null,
+    workouts: [],
 
-  startWorkout: () => {
-    set({
-      currentWorkout: newWorkout(),
-    });
-  },
+    startWorkout: () => {
+      set({
+        currentWorkout: newWorkout(),
+      });
+    },
 
-  finshWorkout: () => {
-    const { currentWorkout } = get();
+    finshWorkout: () => {
+      const { currentWorkout } = get();
 
-    if (!currentWorkout) return;
+      if (!currentWorkout) return;
 
-    set(state => ({
-      currentWorkout: null,
-      workouts: [finishedWorkout(currentWorkout), ...state.workouts],
-    }));
-  },
+      set(state => {
+        state.currentWorkout = null;
+        state.workouts.unshift(finishedWorkout(currentWorkout));
+      });
+    },
 
-  addExercise: (name: string) => {
-    const { currentWorkout } = get();
+    addExercise: (name: string) => {
+      const { currentWorkout } = get();
 
-    if (!currentWorkout) {
-      return;
-    }
-    const newExercise = createExercise(name, currentWorkout.id);
+      if (!currentWorkout) {
+        return;
+      }
+      const newExercise = createExercise(name, currentWorkout.id);
 
-    set(state => ({
-      currentWorkout: state.currentWorkout && {
-        ...state.currentWorkout,
-        exercises: [...state.currentWorkout?.exercises, newExercise],
-      },
-    }));
-  },
-}));
+      // set(state => ({
+      //   currentWorkout: state.currentWorkout && {
+      //     ...state.currentWorkout,
+      //     exercises: [...state.currentWorkout?.exercises, newExercise],
+      //   },
+      // }));
+
+      //* Using immer
+      set(state => {
+        state.currentWorkout!.exercises.push(newExercise);
+      });
+    },
+  }))
+);
