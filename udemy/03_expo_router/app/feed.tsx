e0b71e5e69729fc5f781e0a1510cdb1d4f460b8c
soumadip-dev <soumadipmajila@gmail.com>
2026-08-18 11:31:07 +0530

    feat: set up a project for expo router learning

---
+++ udemy/03_expo_router/app/feed.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const FeedPage = () => {
+  return (
+    <View>
+      <Text>feed</Text>
+    </View>
+  );
+};
+
+export default FeedPage;
---
+++ udemy/03_expo_router/app/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>Hello World</Text>
+      <Link href={'/feed'}>Feed</Link>
+      <Link href={'/explore'}>ExplorePage</Link>
+    </View>
+  );
+}
