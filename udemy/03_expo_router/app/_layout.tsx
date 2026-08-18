b75f8d9fd001fa5e1d528e962ab53843aa762008
soumadip-dev <soumadipmajila@gmail.com>
2026-08-18 20:59:36 +0530

    learn: stack navigation in expo router

--- udemy/03_expo_router/app/_layout.tsx
+++ udemy/03_expo_router/app/_layout.tsx
@@ -1,39 +1,53 @@
-// import { Stack } from "expo-router";
-
-// export default function RootLayout() {
-//   return <Stack />;
-// }
-
-import { Slot } from 'expo-router';
-import { StyleSheet, Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.header}>My App Header</Text>
-      <Slot />
-      <Text style={styles.footer}>My App Footer</Text>
-    </SafeAreaView>
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#f4511e',
+        },
+        headerTintColor: '#ffff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+          fontSize: 24,
+        },
+      }}
+    />
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  header: {
-    fontSize: 24,
-    padding: 12,
-    textAlign: 'center',
-  },
-  footer: {
-    fontSize: 24,
-    padding: 12,
-    textAlign: 'center',
-    backgroundColor: '#f1f1f1',
-  },
-});
+
+//* Slot
+// import { Slot } from 'expo-router';
+// import { StyleSheet, Text } from 'react-native';
+// import { SafeAreaView } from 'react-native-safe-area-context';
+
+// export default function RootLayout() {
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <Text style={styles.header}>My App Header</Text>
+//       <Slot />
+//       <Text style={styles.footer}>My App Footer</Text>
+//     </SafeAreaView>
+//   );
+// }
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+//   title: {
+//     fontSize: 24,
+//     fontWeight: 'bold',
+//   },
+//   header: {
+//     fontSize: 24,
+//     padding: 12,
+//     textAlign: 'center',
+//   },
+//   footer: {
+//     fontSize: 24,
+//     padding: 12,
+//     textAlign: 'center',
+//     backgroundColor: '#f1f1f1',
+//   },
+// });
